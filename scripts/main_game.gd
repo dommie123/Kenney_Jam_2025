da93c999a033085730baa378e881ec14c59028d5
Dominick Wiley <dman9532@gmail.com>
2025-07-20 05:17:14 -0600

    Pointy hands and BGM

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -7,7 +7,7 @@ var minigameCompleted: bool;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	coins = 99999; # For Debug purposes
+	coins = 0;
 	minigameCompleted = false;
 
 
